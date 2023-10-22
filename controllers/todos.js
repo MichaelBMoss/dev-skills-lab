89e801b6a2db1dc91c3402bff36b7e80e749f948
MichaelBMoss <michaelbmoss@gmail.com>
2023-10-22 13:29:51 -0700

    make basic setup

---
+++ controllers/todos.js
@@ -0,0 +1,14 @@
+// controllers/todos.js
+
+  // Convention is to name the model in uppercase and singular
+  const Todo = require('../models/todo');
+
+module.exports = {
+    index
+  };
+
+function index(req, res) {
+    res.render('todos/index', {
+      todos: Todo.getAll()
+    });
+  }
